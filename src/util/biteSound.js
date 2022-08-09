c3c1baa1935eedfdc2befca2c80938412800298b
jaikaran <jaikrns@gmail.com>
2022-08-09 14:43:10 +0530

    trying making sound play

--- src/util/biteSound.js
+++ src/util/biteSound.js
@@ -1,4 +1,11 @@
-const snakeBite = new Audio('../../snake-bite.mp3');
+// const snakeBite = new Audio('../../snake-bite.mp3');
+
+const snakeBite = document.createElement("audio");
+snakeBite.src = '../../snake-bite.mp3';
+snakeBite.setAttribute("preload", "auto");
+snakeBite.setAttribute("controls", "none");
+snakeBite.style.display = "none";
+document.body.appendChild(snakeBite);
 
 const snakeBiteSound = () => {
 	snakeBite.play();
