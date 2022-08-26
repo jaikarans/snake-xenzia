b460a46951982ee2c2ab127cca92d6d48a30c357
Jaikarans <jaikrns@gmail.com>
2022-08-26 17:40:42 +0530

    generateFruit() removed recursion calls implemented loop instead

--- src/fruit/generateFruit.js
+++ src/fruit/generateFruit.js
@@ -1,13 +1,17 @@
 const generateFruit = () => {
 	console.log('generateFruit()');
 	let arr = window.arr;
-	let i = Math.floor(Math.random() * arr.length);
-	let j = Math.floor(Math.random() * arr[0].length);
-	if (arr[i][j] == 0){
-		arr[i][j] = 3;
-		return;
+	let i, j;
+	while (true) {
+		i = Math.floor(Math.random() * arr.length);
+		j = Math.floor(Math.random() * arr[0].length);
+		if (arr[i][j] == 0){
+			arr[i][j] = 3;
+			console.log('food generated arr[', i, ', ',j,']');
+			printArr();
+			return;
+		}
 	}
-	else return generateFruit();
 }
 
 export default generateFruit;
